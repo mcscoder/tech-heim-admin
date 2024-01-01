1add67defde0724f8281394bd1e1435584adb928
mcs <maicongson69@gmail.com>
2024-01-01 15:11:46 +0700

    add: navigation for add product page

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,14 +1,16 @@
-import { ProductList, Select } from "@/components/Elements";
+import { Button, ProductList, Select } from "@/components/Elements";
 import { categories } from "@/constants";
 import { useProductContext } from "@/hooks";
+import { useNavigate } from "react-router-dom";
 
 export const Product = () => {
   const { productRows, getProduct } = useProductContext();
+  const navigate = useNavigate();
 
   return (
     <div className="flex flex-col">
       <div className="content-container flex flex-col my-12 gap-4">
-        <div className="flex gap-4">
+        <div className="flex justify-between gap-4">
           <Select
             defaultValue={0}
             onChange={(e) => {
@@ -26,6 +28,12 @@ export const Product = () => {
               );
             })}
           </Select>
+          <Button
+            variant={"fillSecondary"}
+            onClick={() => navigate("/product/add")}
+          >
+            Add Product
+          </Button>
         </div>
         <ProductList productRows={productRows} />
       </div>
