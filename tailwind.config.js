88637bd8d08ee0cd974551285b42694cd14d2c4e
mcs <maicongson69@gmail.com>
2024-01-04 15:52:25 +0700

    add: base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Gray: {
+          Normal: "#E7E7E3",
+          Dark: "#232321",
+          Main: "#70706E",
+          Medium: "#A6A6A6",
+        },
+        White: {
+          FA: "#FAFAFA",
+        },
+        Blue: "#4A69E2",
+        Yellow: "#FFA52F",
+      },
+      fontFamily: {
+        inter: "Inter, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
