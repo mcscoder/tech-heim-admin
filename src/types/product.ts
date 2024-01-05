bf52edd5c3c8773aec1f8e415a78f73ea1a1da01
mcs <maicongson69@gmail.com>
2024-01-05 21:19:02 +0700

    add: new product type

--- src/types/product.ts
+++ src/types/product.ts
@@ -33,3 +33,76 @@ export interface Product {
   productTechnical: ProductTechnical[];
   productGroup: Pick<ProductGroup, "title" | "productType">[];
 }
+
+export interface NewProduct {
+  id?: number;
+  name: string;
+  currentPrice: number;
+  lastPrice: number | null;
+  quantity: number;
+
+  categoryId: number;
+
+  productTypeId: ProductTypeId[];
+
+  productTechnical: NewProductTechnical[];
+
+  productImage: NewProductImage[];
+}
+
+export interface ProductTypeId {
+  id: number;
+}
+
+export interface NewProductImage {
+  imageURL: string;
+}
+
+export interface NewProductTechnical {
+  title: string;
+  description: string;
+}
+
+export type ProductRowResponseType = Pick<
+  NewProduct,
+  "id" | "name" | "currentPrice" | "lastPrice" | "quantity" | "categoryId"
+> & {
+  sold: number;
+};
+
+export interface ProductType {
+  id: number;
+  title: string;
+}
+
+export interface ProductGroup {
+  id: number;
+  title: string;
+  productType: ProductType[];
+}
+
+export interface ProductId {
+  id: number;
+}
+
+export type ProductRequestBody = Pick<
+  NewProduct,
+  "name" | "currentPrice" | "lastPrice" | "quantity" | "categoryId"
+>;
+
+export type ProductTypeRequestBody = Pick<NewProduct, "productTypeId"> &
+  ProductId;
+
+export type ProductTechnicalRequestBody = Pick<NewProduct, "productTechnical"> &
+  ProductId;
+
+export type ProductImageRequestBody = Pick<NewProduct, "productImage"> &
+  ProductId;
+
+export type AddProductGroupRequestBody = Pick<NewProduct, "categoryId"> &
+  Pick<ProductGroup, "title">;
+
+export interface AddProductTypeRequestBody {
+  productGroupId: number;
+  title: string;
+}
